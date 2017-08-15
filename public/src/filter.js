d325c28d1dac4afba91f00d04022cd12a9071b54
livingthdrm <salaonjoseph@gmail.com>
2017-08-15 14:20:43 +0300

    Update filter.js

--- public/src/filter.js
+++ public/src/filter.js
@@ -1,22 +1,25 @@
+/*jslint node:true */
+/*global angular */
+'use strict';
 angular.module("ContactsApp")
-	.filter("labelCase", function (){
-		return function (input){
+	.filter("labelCase", function () {
+		return function (input) {
 			input = input.replace(/([A-Z])/g, ' $1'); //FirstName = First Name
 			return input[0].toUpperCase() + input.slice(1);
 		};
 	})
-	.filter("camelCase", function(){
+	.filter("camelCase", function () {
 		//First Name -> firstName
-		return function (input){
-			return input.toLowerCase().replace(/ (\w)/g, function (match, letter){
+		return function (input) {
+			return input.toLowerCase().replace(/ (\w)/g, function (match, letter) {
 				return letter.toUpperCase();
 			});
 		};
 	})
-	.filter("keyFilter", function (){
-		return function (obj, query){
+	.filter("keyFilter", function () {
+		return function (obj, query) {
 			var result = {};
-			angular.forEach(obj,function (val, key){
+			angular.forEach(obj, function (val, key) {
 				if (key !== query) {
 					result[key] = val;
 				}
