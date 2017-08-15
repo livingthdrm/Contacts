3858180c4a7d8dbbea6628c075a2338a20927990
livingthdrm <salaonjoseph@gmail.com>
2017-08-15 14:20:15 +0300

    Update factories.js

--- public/src/factories.js
+++ public/src/factories.js
@@ -1,32 +1,36 @@
+/*jslint node:true */
+/*global angular */
+'use strict';
 angular.module("ContactsApp")
-	.factory("Contact", function ($resource){
+	.factory("Contact", function ($resource) {
 		return $resource("/api/contact/:id", { id: "@id"}, {
-			"update":{ method: "PUT"}
+			"update": { method: "PUT"}
 		});
 	})
-	.factory("Fields", function ($q, $http, Contact){
+	.factory("Fields", function ($q, $http, Contact) {
 		var url = "/options/displayed_fields",
 			ignore = ["firstName", "lastName", "id", "userId"],
 			allFields = [],
 			deferred = $q.defer(),
 
-			contacts = Contacts.query(function() {
+			contacts = Contact.query(function () {
 				contacts.forEach(function (c) {
 					Object.key(c).forEach(function (k) {
-						if (allFields.indexOf(k) < 0 && ignore.indexOf(k) < 0) allFields.push(k);
+						if (allFields.indexOf(k) < 0 && ignore.indexOf(k) < 0)
+                        allFields.push(k);
 					});
 				});
 				deferred.resolve(allFields);
 			});
-			return {
-				get: function () {
-					return $http.get(url);
-				},
-				set: function (newFields) {
-					return $http.post(url, { fields: newFields });
-				},
-				headers: function () {
-					return deferred.promise;
-				}
-			};
+        return {
+            get: function () {
+				return $http.get(url);
+            },
+            set: function (newFields) {
+				return $http.post(url, { fields: newFields });
+            },
+            headers: function () {
+				return deferred.promise;
+            }
+        };
 	});
